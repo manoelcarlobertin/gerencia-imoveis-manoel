578ea8aa5ed27ffccc2277eff8a4d5cc4b822f77
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-09-10 09:25:41 -0300

    consertei underline 'user_signed_in?'

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def go_to_home
-    if user signed_in?
+    if user_signed_in?
       redirect_to dashboard_path
     else
       redirect_to new_user_session_path
