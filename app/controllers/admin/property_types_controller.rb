4835dca8697b54e6f49d92719582a1421fc8d87d
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-09-11 10:19:34 -0300

    Criada rota admin/property_types

--- app/controllers/admin/property_types_controller.rb
+++ app/controllers/admin/property_types_controller.rb
@@ -1,2 +1,49 @@
-class Admin::PropertyTypesController < ApplicationController
+module Admin
+  class PropertyTypesController < BaseController
+    before_action :load_property_type, only: [:edit, :update, :destroy]
+
+    def index
+      @property_types = PropertyType.order(:name)
+    end
+
+    def new
+      @property_type = PropertyType.new
+    end
+
+    def create
+      @property_type = PropertyType.new property_type_params
+
+      if @property_type.save
+        redirect_to admin_property_types_path, notice: 'Tipo de Imóvel criado com sucesso.'
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @property_type.update property_type_params
+        redirect_to admin_property_types_path, notice: 'Tipo de Imóvel atualizado com sucesso.'
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @property_type.destroy
+      redirect_to admin_property_types_path, notice: 'Tipo de Imóvel removido com sucesso.'
+    end
+
+    private
+
+    def load_property_type
+      @property_type = PropertyType.find params[:id]
+    end
+
+    def property_type_params
+      params.require(:property_type).permit(:name)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_10_120105) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_11_125909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "property_types", force: :cascade do |t|
+    t.string "name", limit: 50, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
