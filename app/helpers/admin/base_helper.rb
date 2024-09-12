33d2dbc52b92ee326bbc41cd19d7e2f935cb3423
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-09-12 11:10:23 -0300

    Actions new edit property_types are done.

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -1,2 +1,17 @@
 module Admin::BaseHelper
+  def resource_collection_title
+    "resources.#{controller_path.gsub("/", ".")}.collection"
+  end
+
+  def resource_edit_title
+    "resources.#{controller_path.gsub("/", ".")}.edit"
+  end
+
+  def resource_new_title
+    "resources.#{controller_path.gsub("/", ".")}.new"
+  end
+
+  def resource_form_title
+    %w[create new].include?(action_name) ? resource_new_title : resource_edit_title
+  end
 end
